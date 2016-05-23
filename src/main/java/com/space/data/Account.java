a6a448e8089883e8e93e0c070fd2abf2001ef54e
Ivan Timofeev <ivan.timofeev@bsc-ideas.com>
2016-05-23 20:47:03 +0300

    Initial commit

---
+++ src/main/java/com/space/data/Account.java
@@ -0,0 +1,56 @@
+package com.space.data;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by Ivan Timofeev on 23.05.2016.
+ */
+
+
+@Document
+public class Account {
+
+	@Id
+	private String id;
+
+	private String login;
+
+	private String email;
+
+	private String password;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+}
